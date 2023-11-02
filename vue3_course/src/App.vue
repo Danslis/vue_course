4b1a8276961b7789e46e2c7e7de6c2a7219f8bce
Dart <Danslis@yandex.ru>
2023-11-02 23:01:40 +0300

    форма создания поста

--- vue3_course/src/App.vue
+++ vue3_course/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>
-        <div>
-            <button @click="addLike">Like</button>
-            <button v-on:click="addDislike">Dislike</button>
-        </div>
-        <div>Кол-во лайков <strong>{{likes}}</strong></div>
-        <div>Кол-во дизлайков <strong>{{dislikes}}</strong></div>
+    <div class="app">
+        <h4>Создание поста</h4>
+        <form @submit.prevent>
+            <input 
+            v-bind:value="title" 
+            @input="inputTitle"
+            class="input" 
+            type="text" 
+            placeholder="Название">
+            <input  
+            v-bind:value="body" 
+            @input="body=$event.target.value"
+            class="input" 
+            type="text" 
+            placeholder="Описание">
+            <button class="btn" @click="createPost">Создать</button>
+        </form>
+        <div class="post" v-for="post in posts" v-bind:key="post.id">
+           <div><strong>Название:</strong> {{post.title}}</div>
+           <div><strong>Описание:</strong> {{post.body}}</div>
+        </div>       
     </div>
 </template>
 
@@ -13,16 +27,28 @@
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 0
+            posts: [
+                { id: 1, title: 'Javascript', body: 'Описание поста'},
+                { id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                { id: 3, title: 'Javascript 3', body: 'Описание поста 3'}
+            ],
+            title: '',
+            body: ''
         }
     },
     methods: {
-        addLike(){
-            this.likes += 1;
+        createPost(){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body
+            }
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
         },
-        addDislike(){
-            this.dislikes += 1;
+        inputTitle(event){
+            this.title = event.target.value;
         }
     }
 }
@@ -30,5 +56,41 @@ export default {
 
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.app{
+    padding: 20px;
+}
+
+
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
+form{
+    display: flex;
+    flex-direction: column;
+}
+
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
 </style>
