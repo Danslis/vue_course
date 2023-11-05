708d02aba6e2431efc3f3c6f5365304bc6f508d0
Dart <Danslis@yandex.ru>
2023-11-05 21:05:21 +0300

    разбил все на компоненты

--- vue3_course/src/App.vue
+++ vue3_course/src/App.vue
@@ -1,56 +1,37 @@
 <template>
     <div class="app">
-        <h4>Создание поста</h4>
-        <form @submit.prevent>
-            <input 
-            v-bind:value="title" 
-            @input="inputTitle"
-            class="input" 
-            type="text" 
-            placeholder="Название">
-            <input  
-            v-bind:value="body" 
-            @input="body=$event.target.value"
-            class="input" 
-            type="text" 
-            placeholder="Описание">
-            <button class="btn" @click="createPost">Создать</button>
-        </form>
-        <div class="post" v-for="post in posts" v-bind:key="post.id">
-           <div><strong>Название:</strong> {{post.title}}</div>
-           <div><strong>Описание:</strong> {{post.body}}</div>
-        </div>       
+        <post-form
+        @create="createPost"
+        />
+        <post-list 
+        v-bind:posts="posts"
+        
+        />
+           
     </div>
 </template>
 
 <script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
 export default {
+    components: {
+        PostList, PostForm
+    },
     data() {
         return {
             posts: [
                 { id: 1, title: 'Javascript', body: 'Описание поста'},
                 { id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
                 { id: 3, title: 'Javascript 3', body: 'Описание поста 3'}
-            ],
-            title: '',
-            body: ''
+            ]          
         }
     },
-    methods: {
-        createPost(){
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
-        },
-        inputTitle(event){
-            this.title = event.target.value;
-        }
-    }
+     methods: {
+        createPost(post){
+           this.posts.push(post);
+        }       
+    }   
 }
 </script>
 
@@ -67,30 +48,4 @@ export default {
 }
 
 
-.post{
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
-
-form{
-    display: flex;
-    flex-direction: column;
-}
-
-.input{
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
-
-.btn{
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-}
 </style>
